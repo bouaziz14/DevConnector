357ff9ed9323ac74e8b40f6035fa9d8317f73857
Bouaziz dhia <bouazizdhia14@yahoo.fr>
2020-12-14 17:42:05 +0100

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 
-    'mongodb+srv://Bouaziz:Bouaziz558899@cluster0.6twve.mongodb.net/test'
-}
+    mongoURI: 'mongodb+srv://Bouaziz:Bouaziz558899@cluster0.6twve.mongodb.net/test',
+    secretOrKey: 'secret'
+};
+
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
+const path = require('path');
 
 
 const app = express();
+
+// Body parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -17,14 +23,32 @@ mongoose
     .connect(db)
     .then(() => console.log('MongoDb Connected'))
     .catch(err => console.log(err));
+mongoose.set('useNewUrlParser', true);
+ 
+// Passport middleware
+app.use(passport.initialize());
 
-app.get('/',(req, res) => res.send('Hello word'));
+// Passport Config 
 
-// use routes
+require('./config/passport')(passport);
+
+
+// Use Routes
 
 app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
+
+// Server static assets if in production 
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const port =process.env.PORT || 5000;
 
